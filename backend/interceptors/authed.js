7b332ddced75b384c439e572da0efde8f8789bb5
albetnov <ryxinfrvr@gmail.com>
2022-11-04 13:24:50 +0700

    Refactor authed interceptors

--- backend/interceptors/authed.js
+++ backend/interceptors/authed.js
@@ -1,24 +1,29 @@
 const { verify } = require("jsonwebtoken");
 const UserRepository = require("../repositories/UserRepository");
 
-const authed = (req, res, next) => {
+const checkForAuthetication = (req) => {
   if ("authorization" in req.headers) {
     const token = req.headers.authorization.split(" ")[1];
 
     if (token !== null) {
-      verify(token, process.env.APP_KEY, async (err, user) => {
-        if (err) return res.status(500).json({ message: "Error ocourred: " + err.message });
+      return verify(token, process.env.APP_KEY, async (err, user) => {
+        if (err) return false;
         req.user = user;
-        console.log(await UserRepository.findById(user.id));
         if (await UserRepository.findById(user.id)) {
-          next();
-        } else {
-          return res.status(403).json({ message: "Not authorized" });
+          return true;
         }
       });
-    } else {
-      return res.status(403).json({ message: "Not authorized" });
     }
+  }
+
+  return false;
+};
+
+const authed = (req, res, next) => {
+  if (checkForAuthetication(req)) {
+    next();
+  } else {
+    return res.status(403).json({ message: "Unauthorized!", status: 403 });
   }
 };
 
